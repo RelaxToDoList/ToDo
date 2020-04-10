3995d3fa5a4e0cd25e1df8195632b82bec06aeaf
FrutlesPoent <frutlespoent@gmail.com>
2020-04-11 00:53:02 +0700

    Add 'sign in' window

---
+++ src/Welcome_Window1/Sign1W.py
@@ -0,0 +1,16 @@
+from PySide import QtCore, QtGui
+import sys
+from Signin import Ui_MainWindow
+
+#Run Application
+app = QtGui.QApplication(sys.argv)
+#Create UI and form of Application
+MainWindow = QtGui.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+#LOGIC HERE!
+
+
+#Run main loop
+sys.exit(app.exec_())
