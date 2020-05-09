2a5d99bd8682be87984a85e97e8be51b7b03b95b
FrutlesPoent <frutlespoent@gmail.com>
2020-05-09 16:19:03 +0700

    Add func of change name and sec_name

--- src/Welcome_Window1/Main_File.py
+++ src/Welcome_Window1/Main_File.py
@@ -51,10 +51,10 @@ class Settings_Menu(QtWidgets.QMainWindow, Ui_Settings):
         first_name = self.firstname_change.text()
         second_name = self.secondname_change.text()
         if first_name:
-            #DBfunctions.update_record('user','First',str(first_name),'User_ID',User_ID)
+            DBfunctions.update_record('user','First',str(first_name),'User_ID',User_ID)
             self.name.setText(first_name)
         if second_name:
-            #DBfunctions.update_record('user','Last',second_name,'User_ID',User_ID)
+            DBfunctions.update_record('user','Last',second_name,'User_ID',User_ID)
             self.secname.setText(second_name)
         self.close_line_edit()
     def back_main(self):
